a5d2fa609045b679d5170c493308ed7647a15603
Steven Lopez <stevenlopez009r@gmail.com>
2025-03-06 22:51:59 -0500

    inicio de componente fleet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import ComponentFilters from './components/ComponentFilters/ComponentFilters'
 import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
      <Header/>
+     <ComponentFilters/>
     </>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,63 +2,95 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 import Clouds1 from "../../assets/home-clouds-fg@2x.webp";
 import Clouds2 from "../../assets/home-clouds-fg@2x (1).webp";
-import Model3D2 from "../Modelo/Modelo3D2";
 import Logo from "../../assets/logo-removebg-preview.png";
 import MenuLateral from "../MenuLateral/MenuLateral";
-import Fleet from "../Fleet/Fleet";
+import Model3D2 from "../Modelo/Modelo3D2";
 
 const Header: React.FC = () => {
   const [cloudPosition, setCloudPosition] = useState(100);
   const [planePosition, setPlanePosition] = useState(0);
   const [fleetPosition, setFleetPosition] = useState(-1700);
-  const [isPlaneMoving, setIsPlaneMoving] = useState(false);
+  const [isPlaneMoving, setIsPlaneMoving] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCloudPosition(prev => (prev <= -100 ? 100 : prev - 1));
+      setCloudPosition((prev) => (prev <= -100 ? 100 : prev - 1));
     }, 500);
+
     return () => clearInterval(interval);
   }, []);
 
   const handleFleetClick = () => {
-    setIsPlaneMoving(true);
-    setPlanePosition(1700); 
-    setTimeout(() => setFleetPosition(0), 1000);
+    setTimeout(() => {
+      setIsPlaneMoving(false);
+      setFleetPosition(0);
+    }, 1000);
+
+    setPlanePosition(1700);
   };
 
   return (
     <div className="header">
       <div className="container-logo">
-        <img src={Logo} alt="" />
+        <img src={Logo} alt="Logo" />
       </div>
+
       <div className="cloud-wrapper">
         <div className="container-clouds">
-          <img src={Clouds1} alt="" />
-          <img src={Clouds1} alt="" />
+          <img src={Clouds1} alt="Clouds" />
+          <img src={Clouds1} alt="Clouds" />
         </div>
         <div className="container-clouds-two">
-          <img src={Clouds2} alt="" />
-          <img src={Clouds2} alt="" />
+          <img src={Clouds2} alt="Clouds" />
+          <img src={Clouds2} alt="Clouds" />
         </div>
         <div className="container-clouds-three">
-          <img src={Clouds2} alt="" />
-          <img src={Clouds2} alt="" />
+          <img src={Clouds2} alt="Clouds" />
+          <img src={Clouds2} alt="Clouds" />
         </div>
       </div>
-      <div className="model-container" style={{ transform: `translateX(${planePosition}px)`, transition: "transform 2s ease-in-out" }}>
-        <Model3D2 />
-      </div>
-      <div  className="fleet-container" style={{ transform: `translateX(${fleetPosition}px)`, transition: "transform 2s ease-in-out" }}>
-        <Fleet/>
-      </div>
-      <div className="text-container" style={{ opacity: isPlaneMoving ? 0 : 1, transition: "opacity 1s ease-in-out" }}>
-        <p>Hi Fly is a leading wet lease and charter <br /> specialist airline operating worldwide</p>
+
+      {isPlaneMoving ? (
+        <div
+          className="model-container"
+          style={{
+            transform: `translateX(${planePosition}px)`,
+            transition: "transform 2s ease-in-out",
+          }}
+        >
+          <Model3D2 />
+        </div>
+      ) : (
+        <div
+          className="fleet-container"
+          style={{
+            transform: `translateX(${fleetPosition}px)`,
+            transition: "transform 2s ease-in-out",
+          }}
+        >
+          <h2>Flota</h2>
+        </div>
+      )}
+
+      <div
+        className="text-container"
+        style={{
+          opacity: isPlaneMoving ? 1 : 0,
+          transition: "opacity 2s ease-in-out",
+        }}
+      >
+        <p>
+          Hi Fly is a leading wet lease and charter <br /> specialist airline
+          operating worldwide
+        </p>
         <p>For Hi Fly Cargo click here</p>
       </div>
-        <MenuLateral onFleetClick={handleFleetClick} />
+
+      <MenuLateral onFleetClick={handleFleetClick} />
     </div>
   );
 };
 
 export default Header;
+
 
