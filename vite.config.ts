063de9eeb1b4675cd168d531fc1fd7ba12be8829
Steven Lopez <stevenlopez009r@gmail.com>
2025-03-16 16:31:32 -0500

    last f commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // Asegura que las rutas sean correctas
+  base: "/TorreDeControl", // Asegura que las rutas sean correctas
   plugins: [react()],
 });
